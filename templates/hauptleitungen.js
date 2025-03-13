9f3c0aecfb0b1365c4ffb5ad221cd998972eee06
Kevin Reuss <kevreuss@gmail.com>
2025-03-13 20:52:51 +0100

    init

---
+++ templates/hauptleitungen.js
@@ -0,0 +1,892 @@
+window.templates = window.templates || {};
+window.templates.hauptleitungen = {
+  drawingContentVersion: 1,
+  shapes: [],
+  wires: [
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 20,
+            column: 30,
+          },
+          endPosition: {
+            row: 53,
+            column: 30,
+          },
+        },
+        {
+          startPosition: {
+            row: 53,
+            column: 30,
+          },
+          endPosition: {
+            row: 53,
+            column: 27,
+          },
+        },
+        {
+          startPosition: {
+            row: 20,
+            column: 30,
+          },
+          endPosition: {
+            row: 20,
+            column: 27,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 19,
+            column: 30,
+          },
+          endPosition: {
+            row: 10,
+            column: 30,
+          },
+        },
+        {
+          startPosition: {
+            row: 10,
+            column: 30,
+          },
+          endPosition: {
+            row: 10,
+            column: 27,
+          },
+        },
+        {
+          startPosition: {
+            row: 19,
+            column: 30,
+          },
+          endPosition: {
+            row: 19,
+            column: 27,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "thick",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 33,
+            column: 28,
+          },
+          endPosition: {
+            row: 21,
+            column: 28,
+          },
+        },
+        {
+          startPosition: {
+            row: 21,
+            column: 28,
+          },
+          endPosition: {
+            row: 21,
+            column: 27,
+          },
+        },
+        {
+          startPosition: {
+            row: 33,
+            column: 28,
+          },
+          endPosition: {
+            row: 33,
+            column: 27,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "thick",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 35,
+            column: 28,
+          },
+          endPosition: {
+            row: 46,
+            column: 28,
+          },
+        },
+        {
+          startPosition: {
+            row: 46,
+            column: 28,
+          },
+          endPosition: {
+            row: 46,
+            column: 27,
+          },
+        },
+        {
+          startPosition: {
+            row: 35,
+            column: 28,
+          },
+          endPosition: {
+            row: 35,
+            column: 27,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "red",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 35,
+            column: 18,
+          },
+          endPosition: {
+            row: 40,
+            column: 18,
+          },
+        },
+        {
+          startPosition: {
+            row: 40,
+            column: 18,
+          },
+          endPosition: {
+            row: 40,
+            column: 15,
+          },
+        },
+        {
+          startPosition: {
+            row: 35,
+            column: 23,
+          },
+          endPosition: {
+            row: 35,
+            column: 18,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "violet",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 59,
+            column: 23,
+          },
+          endPosition: {
+            row: 59,
+            column: 7,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "red",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 28,
+            column: 18,
+          },
+          endPosition: {
+            row: 33,
+            column: 18,
+          },
+        },
+        {
+          startPosition: {
+            row: 33,
+            column: 18,
+          },
+          endPosition: {
+            row: 33,
+            column: 23,
+          },
+        },
+        {
+          startPosition: {
+            row: 28,
+            column: 15,
+          },
+          endPosition: {
+            row: 28,
+            column: 18,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "orange",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 46,
+            column: 7,
+          },
+          endPosition: {
+            row: 46,
+            column: 23,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 19,
+            column: 16,
+          },
+          endPosition: {
+            row: 10,
+            column: 16,
+          },
+        },
+        {
+          startPosition: {
+            row: 10,
+            column: 16,
+          },
+          endPosition: {
+            row: 10,
+            column: 15,
+          },
+        },
+        {
+          startPosition: {
+            row: 19,
+            column: 15,
+          },
+          endPosition: {
+            row: 19,
+            column: 16,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 20,
+            column: 7,
+          },
+          endPosition: {
+            row: 20,
+            column: 11,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "thick",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 20,
+            column: 15,
+          },
+          endPosition: {
+            row: 20,
+            column: 23,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "thick",
+        lineStyle: "solid",
+        colorCode: "black",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 34,
+            column: 32,
+          },
+          endPosition: {
+            row: 34,
+            column: 27,
+          },
+        },
+      ],
+    },
+    {
+      style: {
+        lineThickness: "medium",
+        lineStyle: "solid",
+        colorCode: "red",
+      },
+      wireSegments: [
+        {
+          startPosition: {
+            row: 34,
+            column: 23,
+          },
+          endPosition: {
+            row: 34,
+            column: 15,
+          },
+        },
+      ],
+    },
+  ],
+  notes: [
+    {
+      position: {
+        column: 8.597734016666667,
+        row: 16.2543410527321,
+      },
+      size: {
+        rows: 3.142190394535809,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x2,5 (C16A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 15.251718016666668,
+        row: 14.675244994398764,
+      },
+      size: {
+        rows: 3.083075811202476,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5*6,0 (C32A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 29.206911016666663,
+        row: 14.956737494398762,
+      },
+      size: {
+        rows: 3.083075811202476,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5*6,0 (C32A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 28.068361016666664,
+        row: 29.03386899439876,
+      },
+      size: {
+        rows: 1.1565133112024757,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x16",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 30.860231016666663,
+        row: 32.31024999439877,
+      },
+      size: {
+        rows: 1.1565133112024757,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x25",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 28.08798401666666,
+        row: 37.75818999439876,
+      },
+      size: {
+        rows: 1.1565133112024757,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x16",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 15.507408016666668,
+        row: 31.011086927732094,
+      },
+      size: {
+        rows: 2.757034144535809,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x6,0 (32A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 15.779201016666667,
+        row: 38.337997019398756,
+      },
+      size: {
+        rows: 1.3635445612024757,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "DN63",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 29.20752701666666,
+        row: 47.88917004439877,
+      },
+      size: {
+        rows: 3.1127633112024755,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x4,0 (B25A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 15.689022016666668,
+        row: 24.949803927732095,
+      },
+      size: {
+        rows: 2.757034144535809,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x6,0 (32A)",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 18.04724501666667,
+        row: 18.387980994398763,
+      },
+      size: {
+        rows: 1.1565133112024757,
+        columns: 0.6026666666666666,
+      },
+      displayBorder: false,
+      text: "5x16",
+      textSize: "small",
+    },
+    {
+      position: {
+        column: 0.6108907500000007,
+        row: 13.04848799964191,
+      },
+      size: {
+        rows: 0.05789361071618038,
+        columns: 0.9039999999999997,
+      },
+      displayBorder: false,
+      text: "",
+      textSize: "medium",
+    },
+  ],
+  rectangles: [
+    {
+      position: {
+        row: 18,
+        column: 23,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "UV1 KG",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 18,
+        column: 11,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "UV3 EG",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 5,
+        column: 23,
+      },
+      size: {
+        rows: 6,
+        columns: 4,
+      },
+      text: "UV\nGartenhaus",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 18,
+        column: 3,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "Multisplit\nKlima",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 32,
+        column: 23,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "Zähler-\nanlage",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 41,
+        column: 23,
+      },
+      size: {
+        rows: 8,
+        columns: 4,
+      },
+      text: "PV\nSpeicher + Wechselrichter",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "orange",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 44,
+        column: 3,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "PV\nModule",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "orange",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 26,
+        column: 11,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "Wallbox\nGarage",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "red",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 51,
+        column: 23,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "UV Zisterne",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 32,
+        column: 11,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "CEE\nGarage",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "red",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 38,
+        column: 11,
+      },
+      size: {
+        rows: 6,
+        columns: 4,
+      },
+      text: "Wärmepumpe",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "red",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 32,
+        column: 32,
+      },
+      size: {
+        rows: 4,
+        columns: 3,
+      },
+      text: "EVU\nHAK",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "thick",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 5,
+        column: 11,
+      },
+      size: {
+        rows: 6,
+        columns: 4,
+      },
+      text: "UV Garage\n(Altbestand)",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "black",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 57,
+        column: 23,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "LAN\n+ SAT",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "violet",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+    {
+      position: {
+        row: 57,
+        column: 3,
+      },
+      size: {
+        rows: 4,
+        columns: 4,
+      },
+      text: "SAT",
+      textSize: "medium",
+      itemDesignation: "",
+      itemDesignationText: "",
+      style: {
+        colorCode: "violet",
+        lineThickness: "medium",
+        lineStyle: "solid",
+      },
+    },
+  ],
+  frames: [
+    {
+      position: {
+        row: 1,
+        column: 2,
+      },
+      size: {
+        rows: 61,
+        columns: 6,
+      },
+      text: "DG",
+      textSize: "large",
+      style: {
+        colorCode: "blue",
+        lineThickness: "thick",
+        lineStyle: "pattern-1",
+      },
+    },
+    {
+      position: {
+        row: 1,
+        column: 10,
+      },
+      size: {
+        rows: 61,
+        columns: 10,
+      },
+      text: "EG",
+      textSize: "large",
+      style: {
+        colorCode: "blue",
+        lineThickness: "thick",
+        lineStyle: "pattern-1",
+      },
+    },
+    {
+      position: {
+        row: 1,
+        column: 22,
+      },
+      size: {
+        rows: 61,
+        columns: 14,
+      },
+      text: "KG",
+      textSize: "large",
+      style: {
+        colorCode: "blue",
+        lineThickness: "thick",
+        lineStyle: "pattern-1",
+      },
+    },
+  ],
+};
